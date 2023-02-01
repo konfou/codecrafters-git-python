ddf3ac9796d9f7eb09c80cfc96f1828a094a7c96
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-02-01 21:38:50 +0200

    stage 2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 import sys
 import os
+import zlib
+
+
+def cat_file(opt, obj):
+    if opt == "-p":
+        with open(f".git/objects/{obj[:2]}/{obj[2:]}", "rb") as blob:
+            data = zlib.decompress(blob.read())
+            text = data.decode().split('\x00')
+            print(text[1], end="")
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     command = sys.argv[1]
     if command == "init":
@@ -14,6 +23,11 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        if len(sys.argv) != 4:
+            print("fatal: two arguments required")
+            return
+        cat_file(sys.argv[2], sys.argv[3])
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
