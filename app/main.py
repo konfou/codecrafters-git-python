157587b44e912971cf9f0693b8d97032083c4c39
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-02-05 23:26:07 +0200

    stage 3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-import sys
+import hashlib
 import os
+import sys
 import zlib
 
 
@@ -7,8 +8,25 @@ def cat_file(opt, obj):
     if opt == "-p":
         with open(f".git/objects/{obj[:2]}/{obj[2:]}", "rb") as blob:
             data = zlib.decompress(blob.read())
-            text = data.decode().split('\x00')
-            print(text[1], end="")
+            _, contents = data.split(b'\x00')
+            print(contents.decode(), end="")
+
+
+def hash_object(opt, path):
+    if opt == "-w":
+        with open(path, "rb") as f:
+            contents = f.read()
+
+        data = (b'blob %d\x00' % len(contents)) + contents
+        obj = hashlib.sha1(data).hexdigest()
+
+        if not os.path.exists(f".git/objects/{obj[:2]}"):
+            os.makedirs(f".git/objects/{obj[:2]}")
+
+        with open(f".git/objects/{obj[:2]}/{obj[2:]}", "wb") as blob:
+            blob.write(zlib.compress(data))
+
+        print(obj, end="")
 
 
 def main():
@@ -28,6 +46,11 @@ def main():
             print("fatal: two arguments required")
             return
         cat_file(sys.argv[2], sys.argv[3])
+    elif command == "hash-object":
+        if len(sys.argv) != 4:
+            print("fatal: two arguments required")
+            return
+        hash_object(sys.argv[2], sys.argv[3])
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
