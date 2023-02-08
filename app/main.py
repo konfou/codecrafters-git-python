1c837b9bbe9f772466a6165d121d8792dbaaf543
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-02-08 02:38:42 +0200

    stage 5 and modifications to previous
    - expand cat-file to support trees by calling ls-tree
    - refactor hash-object so it has optional arguments
      that way can be used by write-tree to hash blobs
    - add to hash-object some error handling
    - refactor ls-tree so it has optional arguments
    - change ls-tree parsing algo from split to buffered partition
      new algo fixes problematic parsing in some cases
      new algo also allows full output rather name-only

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import binascii
 import hashlib
 import os
 import sys
@@ -17,44 +18,117 @@ def obj_path(obj, mknew=False):
 
 def cat_file(opt, obj):
     if opt == "-p":
-        with open(obj_path(obj), "rb") as blob:
-            data = zlib.decompress(blob.read())
-            _, contents = data.split(b'\x00')
-            print(contents.decode(), end="")
+        with open(obj_path(obj), "rb") as f:
+            data = zlib.decompress(f.read())
+            ftlen, _, contents = data.partition(b'\x00')
+            ftype, _ = ftlen.split()
+            if ftype == b"blob":
+                print(contents.decode(), end="")
+            elif ftype == b"tree":
+                ls_tree(obj)
+            else:
+                print(f"error: object `{opt}' has unknown type {ftype}")
     elif opt[0] == '-':
         print(f"error: unknown switch `{opt}'")
 
 
-def hash_object(opt, path):
-    if opt == "-w":
-        if not os.path.exists(path):
-            sys.exit()
+def hash_object(opts):
+    write = False
+    path = None
 
-        with open(path, "rb") as f:
-            contents = f.read()
+    if not isinstance(opts, list):
+        opts = [opts]
 
-        data = (b'blob %d\x00' % len(contents)) + contents
-        obj = hashlib.sha1(data).hexdigest()
+    for opt in opts:
+        if opt == "-w":
+            write = True
+        elif opt[0] == "-":
+            sys.exit(f"error: unknown switch `{opt}'")
+        else:
+            path = opt
 
+    if not os.path.exists(path):
+        sys.exit(f"fatal: could not open '{path}' for reading: No such file or directory")
+
+    if os.path.isdir(path):
+        sys.exit(f"fatal: Unable to hash {path}")
+
+    with open(path, "rb") as f:
+        contents = f.read()
+
+    data = (b'blob %d\x00' % len(contents)) + contents
+    obj = hashlib.sha1(data).hexdigest()
+
+    if write:
         with open(obj_path(obj, mknew=True), "wb") as blob:
             blob.write(zlib.compress(data))
 
-        print(obj, end="")
-    elif opt[0] == '-':
-        print(f"error: unknown switch `{opt}'")
+    return obj
 
 
-def ls_tree(opt, obj):
-    if opt == "--name-only":
-        with open(obj_path(obj), "rb") as tree:
-            data = zlib.decompress(tree.read())
-            _, _, contents = data.partition(b'\x00')
+def ls_tree(opts):
+    name_only = False
+    obj = None
 
-        entries_names = (x.split(b' ')[-1].decode() for x in
-                         contents.split(b'\x00')[:-1])
-        print(*entries_names, sep='\n')
-    elif opt[0] == '-':
-        print(f"error: unknown switch `{opt}'")
+    if not isinstance(opts, list):
+        opts = [opts]
+
+    for opt in opts:
+        if opt == "--name-only":
+            name_only = True
+        elif opt[0] == "-":
+            sys.exit(f"error: unknown switch `{opt}'")
+        else:
+            obj = opt
+
+    with open(obj_path(obj), "rb") as tree:
+        data = zlib.decompress(tree.read())
+        _, _, contents = data.partition(b'\x00')
+
+    while contents:
+        pre, _, contents = contents.partition(b'\x00')
+        sha, contents = contents[:20], contents[20:]
+
+        if name_only:
+            pre = pre.split(b' ')
+            line = pre[1]
+        else:
+            if pre[:1] == b'4':
+                pre = b'0' + pre
+                ftype = b"tree"
+            else:
+                ftype = b"blob"
+
+            pre = pre.split(b' ')
+            line = pre[0] + b' ' + ftype + b' ' + binascii.hexlify(sha) + b'\t' + pre[1]
+
+        print(line.decode())
+
+
+def write_tree(cwd="."):
+    ls_cwd = os.listdir(cwd)
+    contents = b''
+    for entry in sorted(ls_cwd):
+        if entry == ".git":
+            continue
+        path = os.path.join(cwd, entry)
+        if os.path.isdir(path):
+            mode = b"40000"
+            obj = write_tree(path)
+        else:  # isfile
+            mode = b"100" + oct(os.stat(path).st_mode)[-3:].encode()
+            obj = hash_object(path)
+
+        obj = binascii.unhexlify(obj)
+        contents += mode + b' ' + entry.encode() + b'\x00' + obj
+
+    data = (b'tree %d\x00' % len(contents)) + contents
+    obj = hashlib.sha1(data).hexdigest()
+
+    with open(obj_path(obj, mknew=True), "wb") as tree:
+        tree.write(zlib.compress(data))
+
+    return obj
 
 
 def main():
@@ -75,15 +149,15 @@ def main():
             return
         cat_file(sys.argv[2], sys.argv[3])
     elif command == "hash-object":
-        if len(sys.argv) != 4:
-            print("fatal: two arguments required")
+        if len(sys.argv) < 3:
             return
-        hash_object(sys.argv[2], sys.argv[3])
+        print(hash_object(sys.argv[2:]))
     elif command == "ls-tree":
-        if len(sys.argv) != 4:
-            print("fatal: two arguments required")
+        if len(sys.argv) < 3:
             return
-        ls_tree(sys.argv[2], sys.argv[3])
+        ls_tree(sys.argv[2:])
+    elif command == "write-tree":
+        print(write_tree())
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
